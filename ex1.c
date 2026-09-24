1aac6336943200f68a6f8ebe8a62dc251531b17d
Ana Reis <anahlarissareis@gmail.com>
2026-09-24 19:24:40 -0300

    adicionar exercicios

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    main(){
+    int idade = 27;
+    float altura = 1.73f;
+    char nome[] = "Ana";
+
+    printf("Nome: %s\n", nome);
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+
+    return 0;
+
+    }
